d439e2597a44c8c6200907ae3ef278fcee02a91d
dobobaie <dobobaie@hotmail.fr>
2020-02-12 00:06:58 +0700

    Fix(Service): connect work

--- examples/connect.js
+++ examples/connect.js
@@ -8,9 +8,11 @@ const child = new Child({ debug: true });
 child.connect("http://127.0.0.1:3000").then(async thread => {
   thread.log('[child] - is now connected with PID: ' + thread.getPid());
   
+  thread.emit('Hi back !');
+
   thread.response(content => {
     console.log("[child] - response", content);
-    content === "Hello" ? thread.emit('Hi back !') : thread.emit('Fine and you?');
+    content === "How are you ?" ? thread.emit('Fine and you?') : null;
   });
 });
 
